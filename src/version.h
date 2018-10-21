c9ef46d093f7fff1b5f95f3a90ce740068fff604
TheArchangelDev <thearchangelhs@outlook.com>
2018-10-21 03:33:13 +0300

    The new and improved SARONITE!

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const SARONITE_VERSION_TAG;
+extern const char* const SARONITE_VERSION;
+extern const char* const SARONITE_RELEASE_NAME;
+extern const char* const SARONITE_VERSION_FULL;
